5240b6e255a771f74bfcc7219714eea2b56de1f6
RA1NO3O@Mac <ra1no3o@icloud.com>
2024-10-10 19:30:44 +0800

    update: 后端域名;
    feat: dashboard scaffold;

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,7 @@
   <div class="w-full min-h-screen flex flex-col pt-[90px] overflow-hidden">
     <NuxtLoadingIndicator color="#FF7800FF" />
     <LayoutHeader />
-    <div class="w-full grow lg:bg-[url('images/background_lg.png')] lg:bg-no-repeat bg-fixed">
+    <div class="w-full flex grow lg:bg-[url('images/background_lg.png')] lg:bg-no-repeat bg-fixed justify-center">
       <slot />
     </div>
   </div>
--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,15 +1,47 @@
 <script setup lang="ts">
+const { t } = useI18n();
 
+const selectedTab = ref<'team' | 'self'>('team');
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <div />
-    <div />
-    <div />
+  <div class="flex flex-col items-center pt-[40px] max-w-[600px]">
+    <div class="window flex justify-between p-[4px] space-x-[4px]">
+      <button
+        class="tab"
+        :class="selectedTab === 'team' ? 'tab-selected' : ''"
+        @click="selectedTab = 'team'"
+      >
+        {{ t('teamPower') }}
+      </button>
+      <button
+        class="tab"
+        :class="selectedTab === 'self' ? 'tab-selected' : ''"
+        @click="selectedTab = 'self'"
+      >
+        {{ t('personalPower') }}
+      </button>
+    </div>
+    <div class="mt-[28px] window w-[600px] h-[590px]" />
+    <div class="mt-[28px] window w-[600px] h-[574px]" />
   </div>
 </template>
 
-<style scoped lang="scss">
+<style scoped lang="postcss">
+.window {
+  border-radius: 6px;
+  border: 1px solid white;
+  background-color: black;
+  box-shadow: 4px 4px 0 0 white;
 
+  .tab {
+    padding: 12px 35px;
+  }
+
+  .tab-selected {
+    color: black;
+    background-color: white;
+    border-radius: 6px;
+  }
+}
 </style>
