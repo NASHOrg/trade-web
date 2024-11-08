dff579fa1073d6738993611b3b6db8eff1f04a5f
stonega <xijieyin@gmail.com>
2024-11-08 14:37:11 +0800

    refactor: clean unsed assets

--- app/blocks/trade/history/TradeHistoryCompleted.vue
+++ app/blocks/trade/history/TradeHistoryCompleted.vue
@@ -114,8 +114,8 @@ function onSelect(info: (typeof datas.value)[number]) {
         class="mb-4 flex justify-center"
         src="images/empty_box.png"
         densities="1x 2x"
-        height="68"
-        width="80"
+        height="72"
+        width="72"
       />
       <div class="text-sm font-medium text-gray-500">
         No transactions
