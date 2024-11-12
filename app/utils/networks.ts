34939783ce37125ae204d235a17b756c1784abef
stonega <xijieyin@gmail.com>
2024-11-12 16:10:29 +0800

    fix: update trade contract

--- app/utils/networks.ts
+++ app/utils/networks.ts
@@ -9,7 +9,7 @@ export const betaTestnet = {
   feeRate: 3000,
   explorer: 'https://beta-testnet.boolscan.com',
   contracts: {
-    trade: '0xcBdD434B6eCccdBD5DD4264FD7473A38EfFbE294',
+    trade: '0x938b59e1736103Cb44A82dA2e789fD725e4d7E3d',
   },
   tokens: {
     bool: {
