ec37355dece1b231966c260ce6765a3daec3250a
stonega <xijieyin@gmail.com>
2024-11-14 12:21:11 +0800

    fix: price chage

--- app/blocks/trade/TradeChart.vue
+++ app/blocks/trade/TradeChart.vue
@@ -15,8 +15,8 @@ const timePriceForToken = computed(() => {
   const low = BN.min(...items.slice(0, 24).map(item => BN(item.lowPrice)));
   const vol = items.slice(0, 24).reduce((acc, item) => acc.plus(BN(item.tradeAmount)), BN(0));
   return [
-    { id: '24H-high', label: '24H High', value: high.dp(5, 1).toString() },
-    { id: '24H-low', label: '24H Low', value: low.dp(5, 1).toString() },
+    { id: '24H-high', label: '24H High', value: formatAmount(high.toString(), 5, { endPad: true }) },
+    { id: '24H-low', label: '24H Low', value: formatAmount(low.toString(), 5, { endPad: true }) },
     { id: '24H-vol', label: '24H Vol', value: vol.dp(2).toString() },
   ];
 });
@@ -26,8 +26,8 @@ const priceChange = computed(() => {
   const items = tradeData.value.items as any[];
   const dailyData = items.slice(0, 24);
   const close = dailyData[0].closePrice;
-  const open = dailyData[dailyData.length - 1].closePrice;
-  const price = BN(open).minus(close).div(open).times(100).dp(2, 1).toString();
+  const open = dailyData[dailyData.length - 1].openPrice;
+  const price = BN(close).minus(open).div(open).times(100).dp(2, 1).toString();
   return Number(price);
 });
 
@@ -74,7 +74,7 @@ function openTokens() {
         <div
           v-for="item in timePriceForToken"
           :key="item.id"
-          class="w-fit md:w-full h-full flex md:flex-col flex-row md:justify-around justify-between space-x-1"
+          class="w-fit md:w-full h-full flex md:flex-col flex-row md:justify-around justify-between space-x-1 md:space-x-0 items-center"
         >
           <span class="text-[#999999] text-nowrap">{{ item.label }}</span>
           <span class="text-white">{{ item.value }}</span>
