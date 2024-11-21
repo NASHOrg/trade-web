192f3056badcebb7bf944502f3f5a940655f997a
stonega <xijieyin@gmail.com>
2024-11-21 15:37:38 +0800

    fix: update trade cotracct

--- app/utils/networks.ts
+++ app/utils/networks.ts
@@ -8,7 +8,7 @@ export const betaTestnet = {
   explorer: 'https://beta-testnet.boolscan.com',
   icon: 'https://bool.network/bool-network-orange.png',
   contracts: {
-    trade: '0xD79d119793f9A7B10A03DFccab6e225fdf901e19',
+    trade: '0x52295088d335EF9BEDc5837b59cA144c1Fd48983',
     consumer: '0x97668c00FdD830cFF4578bD66283E512FE2b9C71',
   },
   tokens: {
